208d9b68b9e57bf2b84f2d1d74216b5b8ac11693
ipos <ivan.posokhin@yandex.ru>
2021-07-11 19:10:46 +0300

    tests: refactoring

--- src/App.test.ts
+++ src/App.test.ts
@@ -27,31 +27,44 @@ afterAll(() => {
   saveFormBaseFx.use(save);
 });
 
+const getValidName = () => {
+  const numFrom2To40 = faker.datatype.number({ min: 2, max: 40 });
+  return faker.datatype.string(numFrom2To40);
+};
+
+const getInvalidName = () => {
+  const numFrom0To1 = faker.datatype.number({ min: 0, max: 1 });
+  const randomShortName = faker.datatype.string(numFrom0To1);
+
+  const numFrom41 = faker.datatype.number({ min: 41 });
+  const randomLongName = faker.datatype.string(numFrom41);
+
+  return faker.random.arrayElement([randomShortName, randomLongName]);
+};
+
+const getValidAge = () => {
+  return faker.datatype.number({ min: 18 });
+};
+
+const getInvalidAge = () => {
+  return faker.datatype.number({ max: 17 });
+};
+
 describe("Rules point 1", () => {
   test("a name is valid when it consists of more than two chars and less than forty chars", () => {
-    const numFrom2To40 = faker.datatype.number({ min: 2, max: 40 });
-    const randomName = faker.datatype.string(numFrom2To40);
+    const validName = getValidName();
 
-    changeName(randomName);
+    changeName(validName);
 
     expect($nameHasFrom2To40Chars.getState()).toBeTruthy();
   });
 
   test("a name is invalid when it consists of less than two chars and more than forty chars", () => {
     // Arrange
-    const numFrom0To1 = faker.datatype.number({ min: 0, max: 1 });
-    const randomShortName = faker.datatype.string(numFrom0To1);
-
-    const numFrom41 = faker.datatype.number({ min: 41 });
-    const randomLongName = faker.datatype.string(numFrom41);
-
-    const randomName = faker.random.arrayElement([
-      randomShortName,
-      randomLongName,
-    ]);
+    const invalidName = getInvalidName();
 
     // Act
-    changeName(randomName);
+    changeName(invalidName);
 
     // Assertion
     expect($nameHasFrom2To40Chars.getState()).toBeFalsy();
@@ -60,10 +73,9 @@ describe("Rules point 1", () => {
 
 describe("Rules point 2", () => {
   test("the output has validation message on invalid name", () => {
-    const numFrom41 = faker.datatype.number({ min: 41 });
-    const invalidRandomName = faker.datatype.string(numFrom41);
+    const invalidName = getInvalidName();
 
-    changeName(invalidRandomName);
+    changeName(invalidName);
 
     expect($output.getState()).toBe("Name should be from 2 to 40 chars");
   });
@@ -71,17 +83,17 @@ describe("Rules point 2", () => {
 
 describe("Rules point 3", () => {
   test("an age is valid when user is adult", () => {
-    const numFrom18 = faker.datatype.number({ min: 18 });
+    const validAge = getValidAge();
 
-    changeAge(numFrom18);
+    changeAge(validAge);
 
     expect($isAdult.getState()).toBeTruthy();
   });
 
   test("an age is invalid when user isn't adult", () => {
-    const numFrom18 = faker.datatype.number({ max: 17 });
+    const invalidAge = getInvalidAge();
 
-    changeAge(numFrom18);
+    changeAge(invalidAge);
 
     expect($isAdult.getState()).toBeFalsy();
   });
@@ -90,14 +102,12 @@ describe("Rules point 3", () => {
 describe("Rules point 4", () => {
   test("the output has validation message on invalid age", () => {
     // Arrange
-    const numFrom2To40 = faker.datatype.number({ min: 2, max: 40 });
-    const validRandomName = faker.datatype.string(numFrom2To40);
-    changeName(validRandomName);
-
-    const invalidRandomAge = faker.datatype.number({ max: 17 });
+    const validName = getValidName();
+    const invalidAge = getInvalidAge();
 
     // Act
-    changeAge(invalidRandomAge);
+    changeName(validName);
+    changeAge(invalidAge);
 
     // Assertion
     expect($output.getState()).toBe("You are too young");
@@ -106,51 +116,57 @@ describe("Rules point 4", () => {
 
 describe("Rules point 5", () => {
   test("an invalid form isn't submitted to server", () => {
+    // Arrange
+    const invalidName = getInvalidName();
+    const invalidAge = getInvalidAge();
+
     const mock = jest.fn();
     saveFormBaseFx.use(mock);
-    const invalidRandomName = faker.datatype.string(
-      faker.datatype.number({ min: 41 })
-    );
-    const invalidRandomAge = faker.datatype.number({ max: 17 });
 
-    changeName(invalidRandomName);
-    changeAge(invalidRandomAge);
+    // Act
+    changeName(invalidName);
+    changeAge(invalidAge);
     submit();
 
+    // Assertion
     expect(mock).toBeCalledTimes(0);
   });
 });
 
 describe("Rules points 6, 8", () => {
   test("a valid form is submitted to server", () => {
+    // Arrange
+    const validName = getValidName();
+    const validAge = getValidAge();
+
     const mock = jest.fn();
     saveFormBaseFx.use(mock);
-    const validRandomName = faker.datatype.string(
-      faker.datatype.number({ min: 2, max: 40 })
-    );
-    const validRandomAge = faker.datatype.number({ min: 18 });
 
-    changeName(validRandomName);
-    changeAge(validRandomAge);
+    // Act
+    changeName(validName);
+    changeAge(validAge);
     submit();
 
+    // Assertion
     expect(mock).toBeCalledTimes(1);
-    expect(mock).toBeCalledWith({ name: validRandomName, age: validRandomAge });
+    expect(mock).toBeCalledWith({ name: validName, age: validAge });
   });
 });
 
 describe("Rules point 7", () => {
   test("the output has question 'Are you ${firstName}. Right?'", () => {
+    // Arrange
+    const validName = getValidName();
+    const validAge = getValidAge();
+
     const mock = jest.fn();
     saveFormBaseFx.use(mock);
-    const validRandomName = faker.datatype.string(
-      faker.datatype.number({ min: 2, max: 40 })
-    );
-    const validRandomAge = faker.datatype.number({ min: 18 });
 
-    changeName(validRandomName);
-    changeAge(validRandomAge);
+    // Act
+    changeName(validName);
+    changeAge(validAge);
 
-    expect($output.getState()).toBe("Are you " + validRandomName + "? Right?");
+    // Assertion
+    expect($output.getState()).toBe("Are you " + validName + "? Right?");
   });
 });
