9591db4657ff45ae0f8d107d59a1e14a10564f6e
ipos <ivan.posokhin@yandex.ru>
2021-07-11 19:56:18 +0300

    test: rewrite components using reflect

--- src/App.tsx
+++ src/App.tsx
@@ -15,75 +15,58 @@ import {
   $output,
 } from "./model";
 
-import { useStore } from "effector-react";
-import { useEvent } from "effector-react/ssr";
+import { reflect } from "@effector/reflect";
 
 import "./App.css";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-const Name: React.FC<InputProps> = (props) => {
-  const name = useStore($name);
-  const change = useEvent(changeName);
-
-  return (
+const Name = reflect<InputProps>({
+  bind: {
+    value: $name,
+    onChange: changeName.prepend((e) => e.target.value),
+  },
+  view: (props) => (
     <div className="formSection">
-      <input
-        {...props}
-        required
-        value={name}
-        onChange={(e) => change(e.target.value)}
-        id="name"
-        placeholder="Vasya"
-      />
+      <input {...props} required id="name" placeholder="Vasya" />
       <label htmlFor="name">Name</label>
     </div>
-  );
-};
+  ),
+});
 
-const Age: React.FC<InputProps> = (props) => {
-  const age = useStore($age);
-  const change = useEvent(changeAge);
-
-  return (
+const Age = reflect<InputProps>({
+  bind: {
+    value: $age,
+    onChange: changeAge.prepend((e) => +e.target.value),
+  },
+  view: (props) => (
     <div className="formSection">
-      <input
-        {...props}
-        onChange={(e) => change(+e.target.value)}
-        value={age}
-        id="age"
-        type="number"
-        min="0"
-        required
-      />
+      <input {...props} id="age" type="number" min="0" required />
       <label htmlFor="age">Age</label>
     </div>
-  );
-};
+  ),
+});
 
-type OutputProps = OutputHTMLAttributes<HTMLElement>;
+type OutputProps = OutputHTMLAttributes<HTMLElement> & { text: string };
 
-const Output: React.FC<OutputProps> = (props) => {
-  const text = useStore($output);
+const Output = reflect<OutputProps>({
+  bind: { text: $output },
+  view: (props) => <output {...props}>Output: {props.text}</output>,
+});
 
-  return <output {...props}>Output: {text}</output>;
-};
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Submit: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
-  const disabled = !useStore($isValid);
-  const submitFn = useEvent(submit);
-
-  return (
-    <button
-      {...props}
-      onClick={() => submitFn()}
-      disabled={disabled}
-      type="button"
-    >
+const Submit = reflect<ButtonProps>({
+  bind: {
+    onClick: submit.prepend(() => {}),
+    disabled: $isValid.map((valid) => !valid),
+  },
+  view: (props) => (
+    <button {...props} type="button">
       Submit
     </button>
-  );
-};
+  ),
+});
 
 const Form: React.FC<FormHTMLAttributes<HTMLFormElement>> = (props) => {
   return (
